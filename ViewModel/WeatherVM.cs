1b1d8da7c8ec328deaa0adb2a9d9bff87bb718e9
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-11-16 20:47:15 +0100

    Added OnPropChange event handler for weatherVM

---
+++ ViewModel/WeatherVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        //Call the method when prop changes i.e when setter is there
+        private string query;
+
+        public string Query
+        {
+            get => query;
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Trigger the prop changed event
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
